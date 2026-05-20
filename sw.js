3674532e4ff3920d93471bf054baffa4d16f8986
ZeeWon <baysuss@gmail.com>
2026-05-20 22:42:35 +0900

    Cache extra response bank for Sioni v3

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "sioni-v3-cache-20260520-1";
-const ASSETS = ["./", "./index.html", "./style.css", "./motions.css", "./responses.js", "./app.js", "./persist.js", "./manifest.json", "./icon.svg"];
+const CACHE_NAME = "sioni-v3-cache-20260520-2";
+const ASSETS = ["./", "./index.html", "./style.css", "./motions.css", "./responses.js", "./responses-extra.js", "./app.js", "./persist.js", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
