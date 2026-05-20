fe4810eb01208827901d0e82c068605349b43a87
ZeeWon <baysuss@gmail.com>
2026-05-20 22:16:26 +0900

    Cache persistence helper in v2

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "sioni-v2-cache-20260520-1";
-const ASSETS = ["./", "./index.html", "./style.css", "./app.js", "./manifest.json", "./icon.svg"];
+const CACHE_NAME = "sioni-v2-cache-20260520-2";
+const ASSETS = ["./", "./index.html", "./style.css", "./app.js", "./persist.js", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
