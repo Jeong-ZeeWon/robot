0ec1b2517040343afb5bd1026af4ea2a2054d549
ZeeWon <baysuss@gmail.com>
2026-05-24 14:13:12 +0900

    Refresh age 10 cache assets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "sioni-v9-cache-20260523-3";
-const ASSETS = ["./", "./index.html", "./style.css?v=9.2.0", "./motions.css?v=9.2.0", "./v9-face.css?v=9.2.0", "./v4.css?v=9.2.0", "./v5.css?v=9.2.0", "./responses.js?v=9.2.0", "./responses-extra.js?v=9.2.0", "./memory-engine.js?v=9.2.0", "./v7.js?v=9.2.0", "./v4.js?v=9.2.0", "./v5.js?v=9.2.0", "./v5-motions.js?v=9.2.0", "./app.js?v=9.2.0", "./v7-life.js?v=9.2.0", "./idle.js?v=9.2.0", "./v5-version.js?v=9.2.0", "./v9-face-engine.js?v=9.2.0", "./persist.js?v=9.2.0", "./manifest.json?v=9.2.0", "./icon.svg"];
+const CACHE_NAME = "sioni-age10-cache-20260524-1";
+const ASSETS = ["./", "./index.html", "./style.css?v=10.0.0", "./motions.css?v=10.0.0", "./v9-face.css?v=10.0.0", "./sioni-10.css?v=10.0.0", "./v4.css?v=10.0.0", "./v5.css?v=10.0.0", "./responses.js?v=10.0.0", "./responses-extra.js?v=10.0.0", "./sioni-10-lines.js?v=10.0.0", "./memory-engine.js?v=10.0.0", "./v7.js?v=10.0.0", "./v4.js?v=10.0.0", "./v5.js?v=10.0.0", "./v5-motions.js?v=10.0.0", "./app.js?v=10.0.0", "./v7-life.js?v=10.0.0", "./idle.js?v=10.0.0", "./v5-version.js?v=10.0.0", "./v9-face-engine.js?v=10.0.0", "./persist.js?v=10.0.0", "./manifest.json?v=10.0.0", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
